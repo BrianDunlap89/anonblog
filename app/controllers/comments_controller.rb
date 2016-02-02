7177a8d80508efcf3414cb39fc225e6d307d1bf1
Brian Dunlap <briandunlap589@gmail.com>
2016-02-02 15:42:53 -0500

    Remove pry

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,6 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    binding.pry
     if @comment.save
       redirect_to @post
     else
